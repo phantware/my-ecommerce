a0d0255161b08ae1b99fe228f4921c9c392c3f9f
phantware <phantmoney2011@gmail.com>
2020-07-28 11:39:49 +0100

    header added for navigation and its styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.components';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
