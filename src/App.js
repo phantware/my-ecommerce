a1f97332c4926b2b304abf57fb8f28a6e10041b5
phantware <phantmoney2011@gmail.com>
2020-07-28 10:57:51 +0100

    collection-item, collection-preview added with their scss styling

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/Homepage.component';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>This is Hart page</h1>
-    </div>
-  );
-};
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
